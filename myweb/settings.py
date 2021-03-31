6c4e612034b788285bd70d2c8dd8c0ed11ee9dfc
whappy233 <wu-mymail@qq.com>
2021-03-31 21:46:29 +0800

    收集静态文件

--- myweb/settings.py
+++ myweb/settings.py
@@ -269,8 +269,8 @@ USE_TZ = False  # 默认值True。若使用了本地时间，必须设为False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-if DEBUG: STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-else: STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 媒体文件
 MEDIA_URL = '/media/'
