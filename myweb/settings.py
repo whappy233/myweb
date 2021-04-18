2653c7afa3c29938846acab9b84ff7f2aae2b484
whappy233 <wu-mymail@qq.com>
2021-04-18 18:59:12 +0800

    啊啊啊

--- myweb/settings.py
+++ myweb/settings.py
@@ -263,8 +263,11 @@ USE_TZ = False  # 默认值True。若使用了本地时间，必须设为False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 # 媒体文件
 MEDIA_URL = '/media/'
